6e96a32065d0bbc3c44ea68c84b1d07a39554377
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-08 23:05:01 +0530

    commiting UC2

--- src/com/bl/login/LoginUser.java
+++ src/com/bl/login/LoginUser.java
@@ -11,18 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginUser extends HttpServlet{
 	public void service(HttpServletRequest httpServletRequest ,HttpServletResponse httpServletResponse) throws IOException {
-//		String name = httpServletRequest.getParameter("userName");
-//		PrintWriter printWriter=httpServletResponse.getWriter();
-//		printWriter.println(name);
-//		
-//		String password= httpServletRequest.getParameter("password");
-//		printWriter.println(password);
-//		
-//		if(name.equalsIgnoreCase("pratik")){
-//			printWriter.println("login successful");
-//		}else{
-//			printWriter.println("login failed");
-//		}
+		String name = httpServletRequest.getParameter("userName");
+		PrintWriter printWriter=httpServletResponse.getWriter();
+		System.out.println("");
+		String password= httpServletRequest.getParameter("password");
+		System.out.println("");
+		if(name.equalsIgnoreCase("pratik")){
+			printWriter.println("<font color=green><h1>login succesful</h1></font>");
+		
+		}else{
+			printWriter.println("<font color=red><h1>login failed</h1></font>");
+		}
 		
 	}
 }
