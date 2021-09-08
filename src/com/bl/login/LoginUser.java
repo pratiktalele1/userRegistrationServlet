17d51b3214c0e70077980d9637e7cad52dc6c76f
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-08 22:22:49 +0530

    UC1

---
+++ src/com/bl/login/LoginUser.java
@@ -0,0 +1,28 @@
+package com.bl.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginUser extends HttpServlet{
+	public void service(HttpServletRequest httpServletRequest ,HttpServletResponse httpServletResponse) throws IOException {
+//		String name = httpServletRequest.getParameter("userName");
+//		PrintWriter printWriter=httpServletResponse.getWriter();
+//		printWriter.println(name);
+//		
+//		String password= httpServletRequest.getParameter("password");
+//		printWriter.println(password);
+//		
+//		if(name.equalsIgnoreCase("pratik")){
+//			printWriter.println("login successful");
+//		}else{
+//			printWriter.println("login failed");
+//		}
+		
+	}
+}
