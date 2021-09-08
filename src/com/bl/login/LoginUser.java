2ec9f13104af6f87fb3b55e72ed6563e2856f50d
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-08 23:49:53 +0530

    commiting UC3

--- src/com/bl/login/LoginUser.java
+++ src/com/bl/login/LoginUser.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginUser extends HttpServlet{
 	public void service(HttpServletRequest httpServletRequest ,HttpServletResponse httpServletResponse) throws IOException {
 		String name = httpServletRequest.getParameter("userName");
@@ -16,12 +19,27 @@ public class LoginUser extends HttpServlet{
 		System.out.println("");
 		String password= httpServletRequest.getParameter("password");
 		System.out.println("");
-		if(name.equalsIgnoreCase("pratik")){
-			printWriter.println("<font color=green><h1>login succesful</h1></font>");
 		
-		}else{
-			printWriter.println("<font color=red><h1>login failed</h1></font>");
+		Pattern pattern=Pattern.compile("^[A-Z]{1}[a-z]");
+		Matcher matcher=pattern.matcher(name);
+		int flag=0;
+		while (matcher.find()) {
+			flag = 1;
 		}
+		if (flag == 1) {
+			System.out.println("valid username");
+			if(name.equalsIgnoreCase("Pratik")){
+				printWriter.println("<font color=green><h1>login succesful</h1></font>");
+			
+			}else{
+				printWriter.println("<font color=red><h1>login failed</h1></font>");
+			}
+		} else {
+			System.out.println("invalid email");
+		}
+		
+		
+		
 		
 	}
 }
